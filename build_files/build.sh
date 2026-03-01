3267083689fa4971417c2e94f490cdc6a46ed4bc
Mohammed Rabil <rabilrbl7@gmail.com>
2026-03-01 07:28:03 +0000

    Revert base image to stable-20260212 and remove plasma-login-manager installation from build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,8 +12,6 @@ set -ouex pipefail
 # this installs a package from fedora repos
 # dnf5 install -y tmux
 
-dnf5 install -y plasma-login-manager
-
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
