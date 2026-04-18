02ab80538c8886530d253e80e01efd13679b646b
rabilrbl <mail@rabil.me>
2026-04-18 15:56:36 +0530

    Revert "feat: add zen-browser and zed to build script"
    This reverts commit 1019f811c217ee2aed4b5ffccef795d992d3d506.

--- build_files/build.sh
+++ build_files/build.sh
@@ -42,7 +42,6 @@ rm -f /usr/lib/systemd/coredump.conf
 
 dnf5 install -y libcap-ng libcap-ng-devel procps-ng procps-ng-devel
 dnf5 install -y cachyos-settings cachyos-ksm-settings --allowerasing
-dnf5 install -y zen-browser zed
 
 tee /usr/lib/systemd/system/ksmd.service >/dev/null <<'EOF'
 [Unit]
